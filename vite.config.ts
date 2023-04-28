99d212e063528a4d8ab1ea7ee853596dff180b57
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-28 16:41:06 +0300

    mui settings

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   },
   plugins: [react()],
   resolve: {
-    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
+    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}],
   }
 })
