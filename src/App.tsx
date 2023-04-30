d40bb4c55b72e1c49b52232c365f1b44910c8970
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-30 23:35:26 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -11,11 +11,18 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          hello
+          <Box width="100%" height="100%" padding="1 rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+            </Routes>
+          </Box>
         </ThemeProvider>
       </BrowserRouter>
     </div>
-    
   );
 }
 
